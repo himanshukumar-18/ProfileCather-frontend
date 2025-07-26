eb3ccee7d1e6a207adb1d084e2e01364b46b83b0
Himanshu Kumar <rajh5343@gmail.com>
2025-07-26 21:55:36 +0530

    add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="w-full flex justify-center items-center h-[5vh] bg-[#111827]">
+      <footer className="w-full flex justify-center items-center h-[5vh] px-[4rem] bg-[#111827] text-center">
         <p className="text-[#E5E7EB] text-[14px]">
           Copyright © 2025 - ProfileCatcher Develop by <a className="underline" href="https://github.com/himanshukumar-18" target="_blank">
             <span>Himanshu Kumar</span>
